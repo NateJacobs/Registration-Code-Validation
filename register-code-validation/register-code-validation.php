53484d0277cebafc188cb3077399ee5702b04582
Nate Jacobs <nate@natejacobs.org>
2011-09-20 01:21:39 -0700

    Add Random Code Generation
    Three new methods added: createRandomString(), validUniqueString(),
    createRandomStringCollection(). The createRandomStringCollection()
    method returns the specified number of codes in a specified length using
    the specified characters.

--- register-code-validation/register-code-validation.php
+++ register-code-validation/register-code-validation.php
@@ -3,10 +3,14 @@
  *	Plugin Name: Registration Code Validation
  *	Plugin URI: 
  *	Description: Takes an array of registration codes and validates new user registrations based upon the code the user enters. If the code is valid and used to register, it is then removed from the list of valid codes.
- *	Version: 0.1
+ *	Version: 0.2
  *	License: GPL V2
  *	Author: Nate Jacobs <nate@natejacobs.org>
  *	Author URI: http://natejacobs.org
+ */
+ 
+/**
+ *	@todo	create an options page where users can choose how many codes to generate.
  */
 
 class RCVUserRegistration
@@ -24,25 +28,101 @@ class RCVUserRegistration
 	 *	Activation
 	 *
 	 *	Only called when plugin is activated. It creates the array of registration codes.
-	 *	If you activate the plugin without changing the codes you will need to run
-	 *	update_option( 'my_registration_codes', $array ) and pass an array of registration codes.
-	 *	You can do this in your functions.php, but remember to delete the function call 
-	 *	after you run it otherwise your codes will never run out.
-	 *
-	 *	@todo		create an options page where users can add an array of codes to use.
+	 *	As of version 0.2 it will only add 10 codes. If you want to add more you need to modify
+	 *	the $number_of_strings variable.
 	 *
 	 *	@author		Nate Jacobs
 	 *	@since		0.1
 	 */
 	public function activation()
 	{
-		$reg_codes = array( 12, 123, 1234, 12345, 123456, 1234567, 12345678, 123456789, 1234567890, 'abc123' );
+		$character_set = 'abcdefghjkmnpqrstuvwxyz23456789#';
+		$string_length = 10;
+		$number_of_strings = 10;
+		$existing_strings = '';
+		$reg_codes = explode( ',', self::createRandomStringCollection( $string_length, $number_of_strings, $character_set, $existing_strings ) );
 		// saves the registration codes in the wp_options table
 		add_option( 'my_registration_codes', $reg_codes );
 	}
 	
 	/** 
-	 *	Add Registratoin Fields
+	 *	Create a Random String
+	 *
+	 *	Generate a random string using the characters and string length passed.
+	 *
+ 	 *	@author		Nate Jacobs
+	 *	@since		0.2
+	 *
+	 *	@param		int		$string_length
+	 *	@param		string 	$character_set
+	 *	@return		string 	$random_string
+	 */
+	private function createRandomString( $string_length, $character_set ) 
+	{
+	  $random_string = array();
+	  for ( $i = 1; $i <= $string_length; $i++ ) 
+	  {
+	    $rand_character = $character_set[rand(0, strlen( $character_set ) - 1)];
+	    $random_string[] = $rand_character;
+	  }
+	  shuffle( $random_string );
+	  return implode( '', $random_string );
+	}
+	
+	/** 
+	 *	Create a valid Unique String
+	 *
+	 *	Ensure random string is unique. 
+	 *	This method is not fully implemented yet. It is in place for random string passthrough.
+	 *	Eventually, it will allow users to input their own custom codes. This will check against them
+	 *	to make sure the plugin string generation will not create identical ones.
+	 *
+ 	 *	@author		Nate Jacobs
+	 *	@since		0.2
+	 *
+	 *	@param		string	$string_collection
+	 *	@param		string 	$new_string
+	 *	@param		string 	$existing_strings
+	 */
+	private function validUniqueString( $string_collection, $new_string, $existing_strings = '' ) 
+	{
+	  if ( !strlen( $string_collection ) && !strlen( $existing_strings ) )
+	    return true;
+		$combined_strings = $string_collection . ", " . $existing_strings;
+	  return ( strlen( strpos( $combined_strings, $new_string ) ) ) ? false : true;
+	}
+	
+	/** 
+	 *	Generate a Collection of Random Strings
+	 *
+	 *	Creates a specified number of random strings using the characters provided.
+	 *
+ 	 *	@author		Nate Jacobs
+	 *	@since		0.2
+	 *
+	 *	@param		int		$string_length
+	 *	@param		int 	$number_of_strings
+	 *	@param		string 	$character_set
+	 *	@param		string 	$existing_strings
+	 *	@return		string	$string_collection
+	 */
+	private function createRandomStringCollection( $string_length, $number_of_strings, $character_set, $existing_strings = '' ) 
+	{
+	  $string_collection = '';
+	  for ( $i = 1; $i <= $number_of_strings; $i++ ) 
+	  {
+	    $random_string = self::createRandomString( $string_length, $character_set );
+	    while (!self::validUniqueString( $string_collection, $random_string, $existing_strings ) ) 
+	    {
+	      $random_string = self::createRandomString( $string_length, $character_set );
+	    }
+	    $string_collection .= ( !strlen($string_collection ) ) ? $random_string : ", " . $random_string;
+	  }
+	  return $string_collection;
+	}
+	
+	/** 
+	 *	Add Registration Fields
 	 *
 	 *	Hook into the registration form and output a new form field.
 	 *
